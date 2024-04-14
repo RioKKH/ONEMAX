176228c8328a0f0f402c0ce2469e281d360c9f35
Ryoichi <ryoichikakehi@gmail.com>
2024-04-14 16:48:57 +0900

    new file:   run_test3.sh

---
+++ run_test3.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source ./libnsys.sh
+
+CWD=$(pwd); readonly CWD
+
+#RUNALL=1
+RUNALL=10
+#RUNALL=30
+#POPULATION=$(seq 32 1 32); readonly POPULATION
+#CHROMOSOME=$(seq 32 1 32); readonly CHROMOSOME
+POPULATION=$(seq 32 32 1024); readonly POPULATION
+CHROMOSOME=$(seq 32 32 1024); readonly CHROMOSOME
+DATETIME=$(date +%Y%m%d-%H%M%S); readonly DATETIME
+readonly PARAMSFILE=${CWD}/onemax.prms
+readonly BACKUPFILE=${CWD}/result_${DATETIME}.csv
+readonly RESULTFILE=${CWD}/result.csv
+
+if [[ -f ${RESULTFILE} ]]; then
+	mv "${RESULTFILE}" "${BACKUPFILE}"
+	rm "${RESULTFILE}"
+fi
+
+for _ in $(seq 1 1 ${RUNALL}); do
+	for pop in ${POPULATION}; do
+		sed -i "s/^POPSIZE.*$/POPSIZE                   ${pop}/" "${PARAMSFILE}"
+		for chr in ${CHROMOSOME}; do
+			echo "${pop} ${chr}"
+			sed -i "s/^CHROMOSOME.*$/CHROMOSOME                ${chr}/" "${PARAMSFILE}"
+			run_nsight_systems gpuonemax "${pop}_${chr}_${DATETIME}" > "${pop}_${chr}_${DATETIME}.log"
+			#./gpuonemax >> "${BACKUPFILE}"
+		done
+	done
+done
+
+
+
